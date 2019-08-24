7cb329d54872712aa7c1deaab216fbe3bf6f3294
okaryo <okaryotr@gmail.com>
2019-08-24 20:48:44 +0900

    [add] new practices

--- Ruby/practice3.rb
+++ Ruby/practice3.rb
@@ -1,6 +1,26 @@
-numbers = [1, 2, 3, 4, 5, 6]
-sum = 0
-numbers.each do |n|
-  sum += n
+# numbers = [1, 2, 3, 4, 5, 6]
+# sum = 0
+# numbers.each do |n|
+#   sum += n
+# end
+# puts sum
+
+# sum = 0
+# menus = {'a': 100, 'b': 200, 'c': 500}
+# menus.each do |menu|
+#   sum += menu[1]
+# end
+# puts sum
+
+# ar = ["Ruby", "PHP", "Python", "Perl"]
+# sorted = ar.sort { |a, b| a <=> b }
+# p sorted
+# ar = ["Ruby", "PHP", "Python", "Perl"]
+# sorted = ar.sort { |a, b| b <=> a }
+# p sorted
+
+hello = Proc.new do |name|
+  puts "Hello, #{name}!"
 end
-puts sum
+hello.call("Ruby")
+hello.call("World")
