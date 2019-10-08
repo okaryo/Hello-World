4a05a5a26610df38dc4908391651212ad5120473
okaryo <okaryotr@gmail.com>
2019-10-08 22:23:29 +0900

    [add] vue practice

--- VuePractice/chap2/main.js
+++ VuePractice/chap2/main.js
@@ -1,6 +1,12 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: ['Vue.js', 'React.js']
+    message: ['Vue.js', 'React.js'],
+    count: 0
+  },
+  methods: {
+    increment: function() {
+      this.count ++;
+    }
   }
 })
