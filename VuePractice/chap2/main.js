6c5aa7603b969e4307a20491362732282c6c9c8d
okaryo <okaryotr@gmail.com>
2019-10-09 22:26:51 +0900

    [add] new practice

--- VuePractice/chap2/main.js
+++ VuePractice/chap2/main.js
@@ -2,7 +2,12 @@ var app = new Vue({
   el: '#app',
   data: {
     message: ['Vue.js', 'React.js'],
-    count: 0
+    count: 0,
+    isChild: true,
+    isActive: true,
+    textColor: 'blue',
+    bgColor: 'lightgray',
+    radius: 50
   },
   methods: {
     increment: function() {
