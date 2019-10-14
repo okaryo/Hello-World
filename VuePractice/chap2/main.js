97564b8c45d459f81ccac03bf44635cdad6bb13e
okaryo <okaryotr@gmail.com>
2019-10-15 00:26:04 +0900

    [add] new vue practice

--- VuePractice/chap2/main.js
+++ VuePractice/chap2/main.js
@@ -7,11 +7,36 @@ var app = new Vue({
     isActive: true,
     textColor: 'blue',
     bgColor: 'lightgray',
-    radius: 50
+    radius: 50,
+    ok: false,
+    flag: false,
+    name: 'oka',
+    list: [
+      { id: 1, name: 'slime', hp: 5 },
+      { id: 2, name: 'lion', hp: 500 },
+      { id: 3, name: 'dragon', hp: 50000 }
+    ],
+    htmlMessage: 'Hello <strong>Vue.js!</strong>'
   },
   methods: {
     increment: function() {
       this.count ++;
+    },
+    doAdd: function() {
+      var max = this.list.reduce(function(a, b) {
+        return a > b.id ? a : b.id
+      }, 0)
+      this.list.push({
+        id: max + 1,
+        name: this.name,
+        hp: 500
+      })
+    },
+    doRemove: function(index) {
+      this.list.splice(index, 1)
+    },
+    doAttack: function(index) {
+      this.list[index].hp -= 10
     }
   }
 })
