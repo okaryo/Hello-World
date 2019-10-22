706e043f4ea354e93cee4c66501d678823ef653a
okaryo <okaryotr@gmail.com>
2019-10-22 12:19:00 +0900

    [add] new practice

--- VuePractice/chap3/main.js
+++ VuePractice/chap3/main.js
@@ -1,6 +1,19 @@
 var app = new Vue({
   el: '#app',
   data: {
+    message: 'Hello!',
+    textmessage: 'i am kenta',
+    val: false,
+    radioval: '',
+    range: 50,
+    scrollY: 0,
+    timer: null
+  },
+  created: function() {
+    window.addEventListener('scroll', this.handleScroll)
+  },
+  beforeDestroy: function() {
+    window.removeEventListener('scroll', this.handleScroll)
   },
   methods: {
     handleClick: function() {
@@ -8,6 +21,15 @@ var app = new Vue({
     },
     handler: function(comment) {
       console.log(comment)
+    },
+    handleScroll: function() {
+      if (this.timer === null) {
+        this.timer = setTimeout(function() {
+          this.scrollY = window.scrollY
+          clearTimeout(this.timer)
+          this.timer = null
+        }.bind(this), 200)
+      }
     }
   }
 })
