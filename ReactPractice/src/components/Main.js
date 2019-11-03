16aa3283afe31fea196cc6fb2b94e0833b8a8e20
okaryo <okaryotr@gmail.com>
2019-11-04 00:37:41 +0900

    [add] new practice

---
+++ ReactPractice/src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div className="main-wrapper">
+        <p>hello</p>
+      </div>
+    );
+  }
+}
+
+export default Main;
