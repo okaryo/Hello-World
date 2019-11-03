5a773c89972667dafed7f7732ca14c7ab8294c73
okaryo <okaryotr@gmail.com>
2019-11-04 00:35:30 +0900

    [add] ruby practice

--- BashPractice/hello.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ "$1" = "hello" ]; then
-  echo "hi"
-else
-  echo "yes"
-fi
--- VSCodePractice/SamplaClock/main.js
+++
@@ -1,6 +0,0 @@
-{
-  setInterval(() => {
-    const timeText = new Date().toLocaleString("ja-JP");
-    document.querySelector("#currentTime").textContent = timeText;
-  }, 1000)
-}
